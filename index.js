a3d2645a21b5444ea0e8b82129ebd45e86894ac7
Cecilia Serrano <serranomartincecilia15@gmail.com>
2026-04-28 11:15:37 +0200

    colecciones añadidas

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ app.use(express.json());
 app.listen(port, () => {
     console.log("Servidor abierto");
 });
+let series=[ {titulo: "The Bridgerton", genero:"Romance", temporadas: 4, plataforma: "Netflix", estreno: 2020, finalizada: false, director:"Chris Van Dusen", nota: 9},
+    {titulo: "Sex And The City", genero:"Romance", temporadas: 6, plataforma: "Netflix", estreno: 1998, finalizada: true, director:"Darren Star", nota: 9},
+    {titulo: "Never Have I Ever", genero:"Comedia", temporadas: 4, plataforma: "Netflix", estreno: 2020, finalizada: true, director:"Mindy Kaling", nota: 10},
+    {titulo: "Gossip Girl", genero:"Romance", temporadas: 6, plataforma: "Netflix", estreno: 2007, finalizada: true, director:" Stephanie Savage", nota: 6},
+    {titulo: "Aquí no hay quien viva", genero:"Comedia", temporadas: 5, plataforma: "Netflix", estreno: 2003, finalizada: true, director:"Alberto Caballero", nota: 10},
+]
